5750dfdaead59fbc3a9aa2136c5cf39af08fd868
d-rens <displscord@gmail.com>
2023-07-31 18:59:06 +0200

    catppuccin mocha color, font to normal font

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=13"
+	"JetBrains Mono:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+                /*     fg         bg       */
+	[SchemeNorm] = { "#cdd6f4", "#11111b" },
+	[SchemeSel] = { "#ffffff", "#89dceb" },
+	[SchemeOut] = { "#000000", "#b4befe" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
